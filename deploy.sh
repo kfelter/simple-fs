c9030812cb576699512c4369c0442ad8993d273a
kyle felter <felterkyle94@gmail.com>
2019-07-23 01:06:21 -0400

    improvements

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 EC2ADDR=18.234.196.95
+rm info.log
+echo uploading
+ls -1
+echo
 echo build the go bin
 GOOS=linux go build -o portfolio.linux
+echo
 echo attempt to deploy website to ec2 at $EC2ADDR
 scp -r * ubuntu@$EC2ADDR:/home/ubuntu/simple-fs/
